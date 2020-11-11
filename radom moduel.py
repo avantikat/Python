eef207b40c6ad9563a74429dd869bb5a451431ac
avantika tellawar <68954508+avantikat@users.noreply.github.com>
2020-11-11 10:45:17 +0530

    Update and rename radom moduel to radom moduel.py

--- radom moduel.py
+++ radom moduel.py
@@ -3,6 +3,6 @@ random_number = random.randint(0, 1)
 # print(random_number)
 rand = random.random() *100
 # print(rand)
-lst = ["Star Plus", "DD1", "Aaj Tak", "CodeWithHarry"]
+lst = ["Star Plus", "DD1", "Aaj Tak", "colours"]
 choice = random.choice(lst)
 print(choice)
