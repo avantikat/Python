e01a169eabc0eee2ddd97344ee49d6a4e491c1a1
avantika tellawar <68954508+avantikat@users.noreply.github.com>
2020-12-15 17:59:59 +0530

    Update and rename diet and excerice file programme to diet and excerice file programme.py

--- diet and excerice file programme.py
+++ diet and excerice file programme.py
@@ -1,5 +1,5 @@
 # Health Management System
-# 3 clients - Harry, Rohan and Hammad
+# 3 clients - avantika, parth and ved
 
 def getdate():
     import datetime
@@ -19,80 +19,80 @@ def take(k):
         c=int(input("enter 1 for excersise and 2 for food"))
         if(c==1):
             value=input("type here\n")
-            with open("harry-ex.txt","a") as op:
+            with open(" avantika-ex.txt","a") as op:
                 op.write(str([str(gettime())])+": "+value+"\n")
             print("successfully written")
         elif(c==2):
             value = input("type here\n")
-            with open("harry-food.txt", "a") as op:
+            with open("avantika-food.txt", "a") as op:
                 op.write(str([str(gettime())]) + ": " + value + "\n")
             print("successfully written")
     elif(k==2):
         c = int(input("enter 1 for excersise and 2 for food"))
         if (c == 1):
             value = input("type here\n")
-            with open("rohan-ex.txt", "a") as op:
+            with open("parth-ex.txt", "a") as op:
                 op.write(str([str(gettime())]) + ": " + value + "\n")
             print("successfully written")
         elif (c == 2):
             value = input("type here\n")
-            with open("rohan-food.txt", "a") as op:
+            with open("parth-food.txt", "a") as op:
                 op.write(str([str(gettime())]) + ": " + value + "\n")
             print("successfully written")
     elif(k==3):
         c = int(input("enter 1 for excersise and 2 for food"))
         if (c == 1):
             value = input("type here\n")
-            with open("hammad-ex.txt", "a") as op:
+            with open("ved-ex.txt", "a") as op:
                 op.write(str([str(gettime())]) + ": " + value + "\n")
             print("successfully written")
         elif (c == 2):
             value = input("type here\n")
-            with open("hammad-food.txt", "a") as op:
+            with open("ved-food.txt", "a") as op:
                 op.write(str([str(gettime())]) + ": " + value + "\n")
             print("successfully written")
     else:
-        print("plz enter valid input (1(harry),2(rohan),3(hammad)")
+        print("plz enter valid input (1(avantika),2(parth),3(ved)")
 def retrieve(k):
     if k==1:
         c=int(input("enter 1 for excersise and 2 for food"))
         if(c==1):
-            with open("harry-ex.txt") as op:
+            with open("avantika-ex.txt") as op:
                 for i in op:
                     print(i,end="")
         elif(c==2):
-            with open("harry-food.txt") as op:
+            with open("avantika-food.txt") as op:
                 for i in op:
                     print(i, end="")
     elif(k==2):
         c = int(input("enter 1 for excersise and 2 for food"))
         if (c == 1):
-            with open("rohan-ex.txt") as op:
+            with open("parth-ex.txt") as op:
                 for i in op:
                     print(i, end="")
         elif (c == 2):
-            with open("rohan-food.txt") as op:
+            with open("parth-food.txt") as op:
                 for i in op:
                     print(i, end="")
     elif(k==3):
         c = int(input("enter 1 for excersise and 2 for food"))
         if (c == 1):
-            with open("hammad-ex.txt") as op:
+            with open("ved-ex.txt") as op:
                 for i in op:
                     print(i, end="")
         elif (c == 2):
-            with open("hammad-food.txt") as op:
+            with open("ved-food.txt") as op:
                 for i in op:
                     print(i, end="")
     else:
-        print("plz enter valid input (harry,rohan,hammad)")
+        print("plz enter valid input (avantika,parth,ved)")
 print("health management system: ")
 a=int(input("Press 1 for log the value and 2 for retrieve "))
 
 if a==1:
-    b = int(input("Press 1 for harry 2 for rohan 3 for hammad "))
+    b = int(input("Press 1 for avantika 2 for parth 3 for ved "))
     take(b)
 else:
-    b = int(input("Press 1 for harry 2 for rohan 3 for hammad "))
+    b = int(input("Press 1 for avantika 2 for parth  3 for ved "))
     retrieve(b)
-=
+
